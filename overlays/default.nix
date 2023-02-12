d577915c9532c1623e2f3a4b359e3b779e8b6bc3
Robin Voetter <robin@voetter.nl>
2023-02-12 10:08:35 +0100

    update kakoune

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,12 +9,12 @@ self: super: {
 
   # Override kakoune to a more recent version so that shift+space works
   kakoune-unwrapped = super.kakoune-unwrapped.overrideAttrs (old: {
-    version = "2022.07.04";
+    version = "2023.02.12";
     src = super.fetchFromGitHub {
         owner = "mawww";
         repo = "kakoune";
-        rev = "167929c15bb159a469220b9b120f70ebe4933cd5";
-        sha256 = "sha256-2p829+biq5awDmkyIXF8/aPZvBVTpA/FhuN4o2zrQNk=";
+        rev = "3150e9b3cd8e61d9bc68245d67822614d4376cf4";
+        sha256 = "sha256-YuoSUk2vjw/waGNRbSdqrCMBiUJWDMCa3iBsFjZCXtM=";
     };
   });
 
