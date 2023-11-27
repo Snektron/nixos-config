e932861e76ee709e37e4c3f53a10f8156e5244dc
Robin Voetter <robin@voetter.nl>
2023-11-27 21:31:35 +0100

    stuff

--- hosts/python/configuration.nix
+++ hosts/python/configuration.nix
@@ -151,17 +151,6 @@
     SystemMaxUse=100M
   '';
 
-  system.activationScripts.diff = {
-    supportsDryActivation = true;
-    text = ''
-      if [[ -e /run/current-system ]]; then
-        echo "--- diff to current-system"
-        ${pkgs.nvd}/bin/nvd --nix-bin-dir=${config.nix.package}/bin diff /run/current-system "$systemConfig"
-        echo "---"
-      fi
-    '';
-  };
-
   ## Users
   users.users.robin = {
     isNormalUser = true;
