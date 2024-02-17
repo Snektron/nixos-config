098c77919c5bb5cdfc2164a8d3f61a808c2bf9de
Robin Voetter <robin@voetter.nl>
2024-02-17 12:06:43 +0100

    add dailout group to users

--- hosts/common/users.nix
+++ hosts/common/users.nix
@@ -4,7 +4,7 @@
       isNormalUser = true;
       description = "Robin Voetter";
       shell = pkgs.fish;
-      extraGroups = [ "wheel" "networkmanager" "video" "render" "docker" ];
+      extraGroups = [ "wheel" "networkmanager" "video" "render" "docker" "dialout" ];
       openssh.authorizedKeys.keys = [
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLaACuxmwEyBP1KX5FfGBcdIvGwrqW1LuRSHOp+pm9lOyUl2lsC81P9COnPgYzCS8xpcuqWsoHMAByoXR1J6LNw= YubiKey #23825474 PIV Slot 9a"
       ];
