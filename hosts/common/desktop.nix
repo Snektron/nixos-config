9290a58f72b18ce597e2c5139512ec77edc157a5
Robin Voetter <robin@voetter.nl>
2024-09-09 10:24:31 +0200

    block headphone microphone + flake update

--- hosts/common/desktop.nix
+++ hosts/common/desktop.nix
@@ -49,6 +49,41 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    # Stop applications from switching to headset mode by just disabling it all together
+    # https://wiki.archlinux.org/title/Bluetooth_headset#Disable_PipeWire_HSP/HFP_profile
+    wireplumber.extraConfig.bluetoothEnhancements = {
+      "wireplumber.settings" = {
+        "bluetooth.autoswitch-to-headset-profile" = false;
+      };
+
+      "monitor.bluez.properties" = {
+        "bluez.enable-sbc-xq" = false;
+        "bluez.enable-msbc" = false;
+        "bluez.roles" = [ "a2dp_sink" ]; # Only enable a2dp
+      };
+
+      "wh-1000xm3-ldac-hq" = {
+        "monitor.bluez.rules" = [
+          {
+            matches = [
+              {
+                # Match any bluetooth device with ids equal to that of a WH-1000XM3
+                "device.name" = "~bluez_card.*";
+                "device.product.id" = "0x0cd3";
+                "device.vendor.id" = "usb:054c";
+              }
+            ];
+            actions = {
+              update-props = {
+                # Set quality to high quality instead of the default of auto
+                "bluez5.a2dp.ldac.quality" = "hq";
+              };
+            };
+          }
+        ];
+      };
+    };
   };
 
   hardware.bluetooth = {
